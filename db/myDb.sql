c364814711205108a0c8eefeed41d137accbf8c1
bdrencher <bdrencher@gmail.com>
2020-01-30 18:18:33 -0500

    fixed spelling error

--- db/myDb.sql
+++ db/myDb.sql
@@ -3,7 +3,7 @@ servings int, prep_time int);
 
 CREATE TABLE meal_plans (id SERIAL PRIMARY KEY, monday int REFERENCES meals(id),
 tuesday int REFERENCES meals(id), wednesday int REFERENCES meals(id), thursday int REFERENCES meals(id),
-friday int REFERENCES meals(id), saturday int REFERNCES meals(id), sunday int REFERENCES meals(id));
+friday int REFERENCES meals(id), saturday int REFERENCES meals(id), sunday int REFERENCES meals(id));
 
 -- These tables are simple and should reduce data redundency
 -- It is intended that meals will be entered into the meals table and the meal_plans
