fa55f9a0fb3f36700c816ed43350a8f867dcee73
bdrencher <bdrencher@gmail.com>
2020-02-01 10:40:37 -0500

    adding support for external css

--- web/index.php
+++ web/index.php
@@ -2,9 +2,10 @@
 <html>
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Meal Planner - Ben Rencher</title>
         <script src="jquery-3.4.1.min.js"></script>
         <script src="mealPlanner.js"></script>
+        <link rel="stylesheet" href="mealPlanner.css">
+        <title>Meal Planner - Ben Rencher</title>
     </head>
     
     <header>
