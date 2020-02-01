7379236edbf6e42d134c2e39a1c664254eb58930
bdrencher <bdrencher@gmail.com>
2020-02-01 12:53:29 -0500

    another try for external stylesheet

--- web/index.php
+++ web/index.php
@@ -3,8 +3,8 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="jquery-3.4.1.min.js"></script>
-        <script src="../mealPlanner.js"></script>
-        <link rel="stylesheet" href="../mealPlanner.css" type="text/css">
+        <script src="mealPlanner.js"></script>
+        <link rel="stylesheet" href="mealPlanner.css" type="text/css">
         <title>Meal Planner - Ben Rencher</title>
     </head>
     
