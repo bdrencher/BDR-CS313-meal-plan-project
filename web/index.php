79306617e018a4eb7611e91bfe816ba5cd4f1324
bdrencher <bdrencher@gmail.com>
2020-02-01 10:35:38 -0500

    adding support for jQuery, responsive page, title

---
+++ web/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Meal Planner - Ben Rencher</title>
+        <script src="jquery-3.4.1.min.js"></script>
+        <script src="mealPlanner.js"></script>
+    </head>
+    
+    <header>
+        <img src="logo.jpg" alt="logo for meal planner">
+        <h1>Meal Planner</h1>
+    </header>
+
+    <body>
+
+    </body>
+
+    <footer>
+
+    </footer>
+</html>
