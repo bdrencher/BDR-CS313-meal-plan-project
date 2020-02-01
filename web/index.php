94fde3ed68e7f789f5b41d460dbcf5655c2f1ce0
bdrencher <bdrencher@gmail.com>
2020-02-01 12:24:56 -0500

    fixed syntax error with link to external css sheet

--- web/index.php
+++ web/index.php
@@ -4,7 +4,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="jquery-3.4.1.min.js"></script>
         <script src="mealPlanner.js"></script>
-        <link rel="stylesheet" href="mealPlanner.css">
+        <link rel="stylesheet" href="mealPlanner.css" type="text/css">
         <title>Meal Planner - Ben Rencher</title>
     </head>
     
