fb94f3e51fa04faa19cce0a4d3dd4230814b5887
bdrencher <bdrencher@gmail.com>
2020-02-08 18:27:57 -0500

    testing for issues with get meal functoin

--- web/getMealPlan.php
+++ web/getMealPlan.php
@@ -2,24 +2,24 @@
 require 'connectToDB.php';
 $db = returnDB();
 
-/* getMeal will extract meal data once an appropriate id has been provided*/
-function getMeal($mealID)
-{
-    $mealDB = returnDB();
-
-    if($mealID == NULL)
+    /* getMeal will extract meal data once an appropriate id has been provided*/
+    function getMeal($mealID)
     {
-        return array("no meal selected");
-    }
-    $mealQuery = $mealDB->prepare('SELECT name, recipe_url, servings, prep_time FROM meals WHERE id=$mealID');
-    $mealQuery->execute();
+        $mealDB = returnDB();
 
-    $mealData = $mealQuery->fetch(PDO::FETCH_ASSOC);
+        if($mealID == NULL)
+        {
+            return array("no meal selected");
+        }
+        $mealQuery = $mealDB->prepare('SELECT name, recipe_url, servings, prep_time FROM meals WHERE id=$mealID');
+        $mealQuery->execute();
 
-    $dataArray = array($mealData['name'], $mealData['recipe_url'], $mealData['servings'], $mealData['prep_time']);
+        $mealData = $mealQuery->fetch(PDO::FETCH_ASSOC);
 
-    return $dataArray;
-} // end of getMeal
+        $dataArray = array($mealData['name'], $mealData['recipe_url'], $mealData['servings'], $mealData['prep_time']);
+
+        return $dataArray;
+    } // end of getMeal
 
 
 // Retrieve data from meal plan
@@ -32,13 +32,13 @@ $mealPlanData = $query->fetch(PDO::FETCH_ASSOC);
 
 // get data for each meal
 $mealPlanName = $mealPlanData['name'];
-$monday       = getMeal($mealPlanData['monday']);
-$tuesday      = getMeal($mealPlanData['tuesday']);
-$wednesday    = getMeal($mealPlanData['wednesday']);
-$thursday     = getMeal($mealPlanData['thursday']);
-$friday       = getMeal($mealPlanData['friday']);
-$saturday     = getMeal($mealPlanData['saturday']);
-$sunday       = getMeal($mealPlanData['sunday']);
+// $monday       = getMeal($mealPlanData['monday']);
+// $tuesday      = getMeal($mealPlanData['tuesday']);
+// $wednesday    = getMeal($mealPlanData['wednesday']);
+// $thursday     = getMeal($mealPlanData['thursday']);
+// $friday       = getMeal($mealPlanData['friday']);
+// $saturday     = getMeal($mealPlanData['saturday']);
+// $sunday       = getMeal($mealPlanData['sunday']);
 
 // send data to front end
 $mealPlanArray = array($mealPlanName, $monday, $tuesday, $wednesday, $thursday, $friday, $saturday, $sunday);
