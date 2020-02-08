2416a4a8c12f64d84b8ba424515b3837646dd7f2
bdrencher <bdrencher@gmail.com>
2020-02-08 16:40:22 -0500

    multiple require statements for the same file is an issue. Uncommenting and testing code

--- web/getMealPlan.php
+++ web/getMealPlan.php
@@ -5,25 +5,25 @@ require 'getMeal.php';
 
 $planID = $_GET['planID'];
 
-// $query = $db->prepare("SELECT name, monday, tuesday, wednesday, thursday, friday, saturday, sunday FROM meal_plans WHERE id=$planID");
-// $query->execute();
+$query = $db->prepare("SELECT name, monday, tuesday, wednesday, thursday, friday, saturday, sunday FROM meal_plans WHERE id=$planID");
+$query->execute();
 
-// $mealPlanData = $statement->fetch(PDO::FETCH_ASSOC);
+$mealPlanData = $statement->fetch(PDO::FETCH_ASSOC);
 
-// $mealPlanName = mealPlanData['name'];
-// $monday       = json_decode(getMeal(mealPlanData['monday']));
-// $tuesday      = json_decode(getMeal(mealPlanData['tuesday']));
-// $wednesday    = json_decode(getMeal(mealPlanData['wednesday']));
-// $thursday     = json_decode(getMeal(mealPlanData['thursday']));
-// $friday       = json_decode(getMeal(mealPlanData['friday']));
-// $saturday     = json_decode(getMeal(mealPlanData['saturday']));
-// $sunday       = json_decode(getMeal(mealPlanData['sunday']));
+$mealPlanName = mealPlanData['name'];
+$monday       = json_decode(getMeal(mealPlanData['monday'], $db));
+$tuesday      = json_decode(getMeal(mealPlanData['tuesday'], $db));
+$wednesday    = json_decode(getMeal(mealPlanData['wednesday'], $db));
+$thursday     = json_decode(getMeal(mealPlanData['thursday'], $db));
+$friday       = json_decode(getMeal(mealPlanData['friday'], $db));
+$saturday     = json_decode(getMeal(mealPlanData['saturday'], $db));
+$sunday       = json_decode(getMeal(mealPlanData['sunday'], $db));
 
-// // get data for each meal
+// get data for each meal
 
-// $mealPlanArray = array($mealPlanName, $monday, $tuesday, $wednesday, $thursday, $friday, $saturday, $sunday);
+$mealPlanArray = array($mealPlanName, $monday, $tuesday, $wednesday, $thursday, $friday, $saturday, $sunday);
 
 
-// echo json_encode($mealPlanArray);
-echo json_encode($planID);
+echo json_encode($mealPlanArray);
+// echo json_encode($planID);
 ?>
