8819e4912ca1dc49c92aa603aace2bc8b7f5de13
bdrencher <bdrencher@gmail.com>
2020-02-08 14:51:15 -0500

    replaced vanilla js http request and onclick function

--- web/mealPlanner.js
+++ web/mealPlanner.js
@@ -1,6 +1,20 @@
-$("loadMealPlanButton").click(function() {
-    $.get("getMealPlan.php", 1), function (response){
-        let data = JSON.parse(response);
-        console.log(data);
-    }
-});
+function mealPlanRequest(mealPlanID)
+{
+    let request = new XMLHttpRequest();
+
+    request.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200)
+        {
+            let data = JSON.parse(request.response);
+            console.log(data);
+        }
+        else
+        {
+            console.log("something went wrong, status: " + request.status);
+        }
+    };
+
+    request.open("POST", "getMealPlan.php?planID=" + mealPlanID, true);
+    request.send();
+    console.log("sent request");
+}
