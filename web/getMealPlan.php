9efec2d051cdfd5772634613d4018074918e4e63
bdrencher <bdrencher@gmail.com>
2020-02-08 16:57:03 -0500

    sytnax error fix

--- web/getMealPlan.php
+++ web/getMealPlan.php
@@ -10,14 +10,14 @@ $query->execute();
 
 $mealPlanData = $query->fetch(PDO::FETCH_ASSOC);
 
-$mealPlanName = mealPlanData['name'];
-// $monday       = json_decode(getMeal(mealPlanData['monday'], $db));
-// $tuesday      = json_decode(getMeal(mealPlanData['tuesday'], $db));
-// $wednesday    = json_decode(getMeal(mealPlanData['wednesday'], $db));
-// $thursday     = json_decode(getMeal(mealPlanData['thursday'], $db));
-// $friday       = json_decode(getMeal(mealPlanData['friday'], $db));
-// $saturday     = json_decode(getMeal(mealPlanData['saturday'], $db));
-// $sunday       = json_decode(getMeal(mealPlanData['sunday'], $db));
+$mealPlanName = $mealPlanData['name'];
+// $monday       = json_decode(getMeal($mealPlanData['monday'], $db));
+// $tuesday      = json_decode(getMeal($mealPlanData['tuesday'], $db));
+// $wednesday    = json_decode(getMeal($mealPlanData['wednesday'], $db));
+// $thursday     = json_decode(getMeal($mealPlanData['thursday'], $db));
+// $friday       = json_decode(getMeal($mealPlanData['friday'], $db));
+// $saturday     = json_decode(getMeal($mealPlanData['saturday'], $db));
+// $sunday       = json_decode(getMeal($mealPlanData['sunday'], $db));
 
 // // get data for each meal
 
