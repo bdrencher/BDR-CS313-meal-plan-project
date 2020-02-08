9c62ba889aab2d2bab84d0f3b4e0e57414cd637a
bdrencher <bdrencher@gmail.com>
2020-02-08 18:18:06 -0500

    changed db call within get meal functoin

--- web/getMealPlan.php
+++ web/getMealPlan.php
@@ -5,18 +5,20 @@ $db = returnDB();
 /* getMeal will extract meal data once an appropriate id has been provided*/
 function getMeal($mealID)
 {
+    $mealDB = returnDB();
+
     if($mealID == NULL)
     {
         return $dataArray = json_encode("No meal provided");
     }
-    $query = $db->prepare('SELECT name, recipe_url, servings, prep_time FROM meals WHERE id=$mealID');
+    $query = $mealDB->prepare('SELECT name, recipe_url, servings, prep_time FROM meals WHERE id=$mealID');
     $query->execute();
 
     $mealData = $query->fetch(PDO::FETCH_ASSOC);
 
     $dataArray = array($mealData['name'], $mealData['recipe_url'], $mealData['servings'], $mealData['prep_time']);
 
-    return json_encode("a string");
+    return json_encode($dataArray);
 } // end of getMeal
 
 
