0e2f81fb013b1bcbe93586939d37c8922e17a155
bdrencher <bdrencher@gmail.com>
2020-02-08 18:14:44 -0500

    changed get meal so the function is defined within getMealPlan

--- web/getMealPlan.php
+++ web/getMealPlan.php
@@ -1,8 +1,26 @@
 <?php
 require 'connectToDB.php';
-require 'getMeal.php';
 $db = returnDB();
 
+/* getMeal will extract meal data once an appropriate id has been provided*/
+function getMeal($mealID)
+{
+    if($mealID == NULL)
+    {
+        return $dataArray = json_encode("No meal provided");
+    }
+    $query = $db->prepare('SELECT name, recipe_url, servings, prep_time FROM meals WHERE id=$mealID');
+    $query->execute();
+
+    $mealData = $query->fetch(PDO::FETCH_ASSOC);
+
+    $dataArray = array($mealData['name'], $mealData['recipe_url'], $mealData['servings'], $mealData['prep_time']);
+
+    return json_encode("a string");
+} // end of getMeal
+
+
+// Retrieve data from meal plan
 $planID = $_GET['planID'];
 
 $query = $db->prepare("SELECT name, monday, tuesday, wednesday, thursday, friday, saturday, sunday FROM meal_plans WHERE id=$planID");
@@ -10,17 +28,17 @@ $query->execute();
 
 $mealPlanData = $query->fetch(PDO::FETCH_ASSOC);
 
+// get data for each meal
 $mealPlanName = $mealPlanData['name'];
-$monday       = json_decode(getMeal($mealPlanData['monday'], $db));
-$tuesday      = json_decode(getMeal($mealPlanData['tuesday'], $db));
-$wednesday    = json_decode(getMeal($mealPlanData['wednesday'], $db));
-$thursday     = json_decode(getMeal($mealPlanData['thursday'], $db));
-$friday       = json_decode(getMeal($mealPlanData['friday'], $db));
-$saturday     = json_decode(getMeal($mealPlanData['saturday'], $db));
-$sunday       = json_decode(getMeal($mealPlanData['sunday'], $db));
+$monday       = json_decode(getMeal($mealPlanData['monday']));
+$tuesday      = json_decode(getMeal($mealPlanData['tuesday']));
+$wednesday    = json_decode(getMeal($mealPlanData['wednesday']));
+$thursday     = json_decode(getMeal($mealPlanData['thursday']));
+$friday       = json_decode(getMeal($mealPlanData['friday']));
+$saturday     = json_decode(getMeal($mealPlanData['saturday']));
+$sunday       = json_decode(getMeal($mealPlanData['sunday']));
 
-// // get data for each meal
-
+// send data to front end
 $mealPlanArray = array($mealPlanName, $monday, $tuesday, $wednesday, $thursday, $friday, $saturday, $sunday);
 
 echo json_encode($mealPlanArray);
