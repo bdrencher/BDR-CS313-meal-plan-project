44150ddcf8726a47f5815b2b5745158065939c64
bdrencher <bdrencher@gmail.com>
2020-02-08 16:59:59 -0500

    testing a single day data retrival

--- web/getMealPlan.php
+++ web/getMealPlan.php
@@ -12,18 +12,18 @@ $mealPlanData = $query->fetch(PDO::FETCH_ASSOC);
 
 $mealPlanName = $mealPlanData['name'];
 $monday       = json_decode(getMeal($mealPlanData['monday'], $db));
-$tuesday      = json_decode(getMeal($mealPlanData['tuesday'], $db));
-$wednesday    = json_decode(getMeal($mealPlanData['wednesday'], $db));
-$thursday     = json_decode(getMeal($mealPlanData['thursday'], $db));
-$friday       = json_decode(getMeal($mealPlanData['friday'], $db));
-$saturday     = json_decode(getMeal($mealPlanData['saturday'], $db));
-$sunday       = json_decode(getMeal($mealPlanData['sunday'], $db));
+// $tuesday      = json_decode(getMeal($mealPlanData['tuesday'], $db));
+// $wednesday    = json_decode(getMeal($mealPlanData['wednesday'], $db));
+// $thursday     = json_decode(getMeal($mealPlanData['thursday'], $db));
+// $friday       = json_decode(getMeal($mealPlanData['friday'], $db));
+// $saturday     = json_decode(getMeal($mealPlanData['saturday'], $db));
+// $sunday       = json_decode(getMeal($mealPlanData['sunday'], $db));
 
 // // get data for each meal
 
 $mealPlanArray = array($mealPlanName, $monday, $tuesday, $wednesday, $thursday, $friday, $saturday, $sunday);
 
 
-echo json_encode($mealPlanArray);
+echo json_encode($monday);
 // echo json_encode($mealPlanName);
 ?>
