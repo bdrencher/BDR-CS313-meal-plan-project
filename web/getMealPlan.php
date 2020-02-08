154a5919a504eb012194e25a52f00d345b60c2a3
bdrencher <bdrencher@gmail.com>
2020-02-08 14:46:31 -0500

    debugging getMealPlan script

--- web/getMealPlan.php
+++ web/getMealPlan.php
@@ -3,26 +3,27 @@ require "connectToDB.php";
 require "getMeal.php";
 $db = returnDB();
 
-$planID = $_POST['planID'];
+// $planID = $_POST['planID'];
 
-$query = $db->prepare("SELECT name, monday, tuesday, wednesday, thursday, friday, saturday, sunday FROM meal_plans WHERE id=$planID");
-$query->execute();
+// $query = $db->prepare("SELECT name, monday, tuesday, wednesday, thursday, friday, saturday, sunday FROM meal_plans WHERE id=$planID");
+// $query->execute();
 
-$mealPlanData = $statement->fetch(PDO::FETCH_ASSOC);
+// $mealPlanData = $statement->fetch(PDO::FETCH_ASSOC);
 
-$mealPlanName = mealPlanData['name'];
-$monday       = json_decode(getMeal(mealPlanData['monday']));
-$tuesday      = json_decode(getMeal(mealPlanData['tuesday']));
-$wednesday    = json_decode(getMeal(mealPlanData['wednesday']));
-$thursday     = json_decode(getMeal(mealPlanData['thursday']));
-$friday       = json_decode(getMeal(mealPlanData['friday']));
-$saturday     = json_decode(getMeal(mealPlanData['saturday']));
-$sunday       = json_decode(getMeal(mealPlanData['sunday']));
+// $mealPlanName = mealPlanData['name'];
+// $monday       = json_decode(getMeal(mealPlanData['monday']));
+// $tuesday      = json_decode(getMeal(mealPlanData['tuesday']));
+// $wednesday    = json_decode(getMeal(mealPlanData['wednesday']));
+// $thursday     = json_decode(getMeal(mealPlanData['thursday']));
+// $friday       = json_decode(getMeal(mealPlanData['friday']));
+// $saturday     = json_decode(getMeal(mealPlanData['saturday']));
+// $sunday       = json_decode(getMeal(mealPlanData['sunday']));
 
-// get data for each meal
+// // get data for each meal
 
-$mealPlanArray = array($mealPlanName, $monday, $tuesday, $wednesday, $thursday, $friday, $saturday, $sunday);
+// $mealPlanArray = array($mealPlanName, $monday, $tuesday, $wednesday, $thursday, $friday, $saturday, $sunday);
 
 
-echo json_encode($mealPlanArray);
+// echo json_encode($mealPlanArray);
+echo json_encode("this is a string");
 ?>
