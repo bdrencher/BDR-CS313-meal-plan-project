754e9fa89ac759725bcf4e2b51b48b13b1c69010
bdrencher <bdrencher@gmail.com>
2020-02-11 17:38:45 -0500

    changed the insert string to use single quotes just in case I'm missing something

--- web/addMeal.php
+++ web/addMeal.php
@@ -7,7 +7,7 @@ $servings = $_POST['servings'];
 $time = $_POST['prepTime'];
 $url = $_POST['url'];
 
-$query = "INSERT INTO meals (name, recipe_url, servings, prep_time) VALUES(:name, :url, :servings, :time)";
+$query = 'INSERT INTO meals (name, recipe_url, servings, prep_time) VALUES(:name, :url, :servings, :time)';
 $statement = $db->prepare($query);
 
 $statement->bindValue(':name', $name);
