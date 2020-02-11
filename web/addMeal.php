6782e0783ab7c33282cf043c8f06d881084cddb6
bdrencher <bdrencher@gmail.com>
2020-02-10 21:07:08 -0500

    fix syntax error in sql query

--- web/addMeal.php
+++ web/addMeal.php
@@ -7,6 +7,6 @@ $url = $_POST['url'];
 $servings = $_POST['servings'];
 $time = $_POST['prepTime'];
 
-$query = $db->prepare("INSERT INTO meals (name, recipe_url, servings, prep_time) VALUES ($name, $url, $servings, $time)");
+$query = $db->prepare("INSERT INTO meals (name, recipe_url, servings, prep_time) VALUES = ($name, $url, $servings, $time)");
 $query->execute();
 ?>
