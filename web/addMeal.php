c5bef36a9769346531e74a3f8405797640342668
bdrencher <bdrencher@gmail.com>
2020-02-10 21:14:28 -0500

    fixing possible syntax error

--- web/addMeal.php
+++ web/addMeal.php
@@ -3,10 +3,10 @@ require "connectToDB.php";
 $db = returnDB();
 
 $name = $_POST['name'];
-$url = $_POST['url'];
 $servings = $_POST['servings'];
 $time = $_POST['prepTime'];
+$url = $_POST['url'];
 
-$query = $db->prepare("INSERT INTO meals (name, recipe_url, servings, prep_time) VALUES = ($name, $url, $servings, $time)");
+$query = $db->prepare("INSERT INTO meals (name, recipe_url, servings, prep_time) VALUES = ('$name', '$url', $servings, $time)");
 $query->execute();
 ?>
