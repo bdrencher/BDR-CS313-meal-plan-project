2f124ead49b1f910248a941b49ef5a1633c5a6fa
bdrencher <bdrencher@gmail.com>
2020-02-10 22:07:28 -0500

    corrected syntax errors in insert query

--- web/addMeal.php
+++ web/addMeal.php
@@ -7,6 +7,15 @@ $servings = $_POST['servings'];
 $time = $_POST['prepTime'];
 $url = $_POST['url'];
 
-$query = $db->prepare("INSERT INTO meals (name, recipe_url, servings, prep_time) VALUES = ('$name', '$url', $servings, $time)");
-$query->execute();
+$query = "INSERT INTO meals (name, recipe_url, servings, prep_time) VALUES(:name, :url, :servings, :time)";
+$statement = $db->prepare($query);
+
+$statement->bindValue(':name', $name);
+$statement->bindValue(':url', $url);
+$statement->bindValue(':servings', $servings);
+$statement->bindValue(':time', $time);
+
+$statement->execute();
+
+return $db->lastInsertId('mealsIdSequence');
 ?>
