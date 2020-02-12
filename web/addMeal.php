f92ec64c2ddf1863c93a8f1a79662791d74da57a
bdrencher <bdrencher@gmail.com>
2020-02-11 20:27:07 -0500

    removed debug statements

--- web/addMeal.php
+++ web/addMeal.php
@@ -7,11 +7,6 @@ $servings = $_POST['servings'];
 $time = $_POST['prepTime'];
 $url = $_POST['url'];
 
-echo $name;
-echo $servings;
-echo $time;
-echo $url;
-
 $query = 'INSERT INTO meals (name, recipe_url, servings, prep_time) VALUES(:name, :url, :servings, :time)';
 $statement = $db->prepare($query);
 
