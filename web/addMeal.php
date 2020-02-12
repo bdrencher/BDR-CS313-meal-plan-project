d0cdfe42873d8ff6b1369ad8f8e484e91ba93894
bdrencher <bdrencher@gmail.com>
2020-02-11 20:46:31 -0500

    added is set parameters, I'm hoping this will prevent non blank values from passing to the server? It may make no difference

--- web/addMeal.php
+++ web/addMeal.php
@@ -2,10 +2,24 @@
 require "connectToDB.php";
 $db = returnDB();
 
-$name = $_POST['name'];
-$servings = $_POST['servings'];
-$time = $_POST['prepTime'];
-$url = $_POST['url'];
+if(isset($_POST['name'])){
+    $name = $_POST['name'];
+}
+
+if(isset($_POST['servings']))
+{
+    $servings = $_POST['servings'];
+}
+
+if(isset($_POST['prepTime']))
+{
+    $time = $_POST['prepTime'];
+}
+
+if(isset($_POST['url']))
+{
+    $url = $_POST['url'];
+}
 
 $query = 'INSERT INTO meals (name, recipe_url, servings, prep_time) VALUES(:name, :url, :servings, :time)';
 $statement = $db->prepare($query);
